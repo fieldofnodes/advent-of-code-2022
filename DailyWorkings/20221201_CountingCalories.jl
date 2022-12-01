0cff9943b723a982b5baace4a40f49a8411506f7
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2022-12-01 15:38:39 +0000

    Day 1 is done

--- DailyWorkings/20221201_CountingCalories.jl
+++ DailyWorkings/20221201_CountingCalories.jl
@@ -11,5 +11,14 @@ data = readdlm(data_path)
 
 f = open(data_path)
 lines = readlines(f)
-[l == "" ? "Empty" : l for l in lines]
+#empty_lines = [l == "" ? "Empty" : l for l in lines]
+empty_indx = findall(==(""),lines)
+lower_bounds = vcat(0,empty_indx)
+sum_cals_per_elf = 
+    [sum(parse.(Int,lines[lower_bounds[i]+1:lower_bounds[i+1]-1])) 
+        for i in 1:eachindex(lower_bounds).stop-1]
+
+top_cals = maximum(sum_cals_per_elf)
+top_3_cals = sort(sum_cals_per_elf,rev=true)[1:3] |> sum
+
 
